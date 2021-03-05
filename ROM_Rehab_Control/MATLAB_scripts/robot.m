33a03f01547b39c03d280e89d40562bc49477bd9
addem26 <adamc1@andrew.cmu.edu>
2021-03-05 17:45:47 -0500

    Need to fix COM and offsets

--- ROM_Rehab_Control/MATLAB_scripts/robot.m
+++ ROM_Rehab_Control/MATLAB_scripts/robot.m
@@ -1,11 +1,30 @@
-body1 = rigidBody('body1'); 
-jnt1 = rigidBodyJoint('jnt1','revolute'); 
-tform = trvec2tform([0.25 0.25 0]); %User defined 
-setFixedTransform(jnt1,tform); 
-body1.Joint = jnt1; 
+l = 0.5; %Define 
+base_link = rigidBody('base_link'); 
+link = rigidBody('link');
+handle = rigidBody('handle'); 
+
+jnt1 = rigidBodyJoint('jnt1','revolute');
+jnt2 = rigidBodyJoint('jnt2','fixed');
+jnt3 = rigidBodyJoint('jnt3', 'fixed');
+
+tform1 = trvec2tform([0.0 0.00 l/6]); %User defined
+tform2 = trvec2tform([l l 0]); 
+tform3 = trvec2tform([0 0 l/2]); 
+
+setFixedTransform(jnt1,tform1); 
+setFixedTransform(jnt2,tform2); 
+setFixedTransform(jnt3,tform3)
+
+base_link.Joint = jnt1; 
+link.Joint = jnt2;
+handle.Joint = jnt3; 
+
+link.CenterOfMass = [l l/4 l]; %Center of link  
 
 robo = rigidBodyTree; 
-addBody(robo,body1,'base'); 
+addBody(robo,base_link,'base');
+addBody(robo,link,'base_link');
+addBody(robo,handle,'link'); 
 showdetails(robo) 
 
 show(robo)  
